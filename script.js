208a65d1de5c149ca9aafebe881ff1983147d292
WardenOfTheCove <wardenscove@gmail.com>
2026-06-22 17:24:37 -0700

    Website files added to main

---
+++ script.js
@@ -0,0 +1,22 @@
+function YouTube() {
+    window.open("https://www.youtube.com/@wardenscove5921", "_blank");
+}
+
+function GitHub() {
+    window.open("https://github.com/WardenOfTheCove", "_blank");
+}
+
+function WardenOfTheCove() {
+    window.location.href = "index.html";
+}
+
+function contacts() {
+    window.location.href = "contact.html";
+}
+
+<<<<<<< HEAD
+=======
+function scroll() {
+    document.getElementById("main").scrollTop
+}
+>>>>>>> a6ef7a6c3e275b907b9c9517b03fa70ec75d654a
